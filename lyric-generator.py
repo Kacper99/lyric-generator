a1aed9174f0909f4df21e6a16150e624a4708f30
Kacper <kacper99@hotmail.co.uk>
2020-06-20 14:51:07 +0100

    Fixed next ID not being updated

--- lyric-generator.py
+++ lyric-generator.py
@@ -5,6 +5,9 @@ import os
 import argparse
 import time
 
+physical_devices = tf.config.list_physical_devices('GPU')
+tf.config.experimental.set_memory_growth(physical_devices[0], enable=True)
+
 
 parser = argparse.ArgumentParser('Generate lyrics')
 parser.add_argument('-s', '--skip', help='Skip training', action='store_true')
@@ -25,16 +28,10 @@ def split_input_target(chunk):
     return input_text, target_text
 
 
-def build_model(vocab_size, embedding_dim, rnn_units, batch_size):
-    model = tf.keras.Sequential([
-        tf.keras.layers.Embedding(vocab_size, embedding_dim, batch_input_shape=[batch_size, None]),
-        tf.keras.layers.GRU(rnn_units, return_sequences=True, stateful=True, recurrent_initializer='glorot_uniform'),
-        tf.keras.layers.Dense(vocab_size)
-    ])
-    return model
 
 
-text = open('data/shakespere.txt').read()
+
+text = open('data/arctic_monkeys-corpus.txt', 'rb').read().decode(encoding='utf-8')
 
 # Get all the unique characters in the text
 vocab = sorted(set(text))
@@ -54,12 +51,12 @@ print('\n}')
 
 # The maximum length sentence we want for a single input in characters
 seq_length = 100
-examples_per_epoch = len(text) // (seq_length + 1)
+examples_per_epoch = len(text)//(seq_length+1)
 
 # Create training sets
 char_dataset = tf.data.Dataset.from_tensor_slices(text_as_int)
 
-sequences = char_dataset.batch(seq_length + 1, drop_remainder=True)
+sequences = char_dataset.batch(seq_length+1, drop_remainder=True)
 
 dataset = sequences.map(split_input_target)
 # for input_example, output_example in dataset.take(1):
@@ -77,6 +74,18 @@ vocab_size = len(vocab)
 embedding_dim = 256
 rnn_units = 1024
 
+def build_model(vocab_size, embedding_dim, rnn_units, batch_size):
+    model = tf.keras.Sequential([
+        tf.keras.layers.Embedding(vocab_size, embedding_dim,
+                                  batch_input_shape=[batch_size, None]),
+        tf.keras.layers.GRU(rnn_units,
+                            return_sequences=True,
+                            stateful=True,
+                            recurrent_initializer='glorot_uniform'),
+        tf.keras.layers.Dense(vocab_size)
+    ])
+    return model
+
 # Configure checkpointspython
 checkpoint_dir = './training_checkpoints'
 checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt_{epoch}")
@@ -85,6 +94,11 @@ checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_pre
 
 if not args.skip:
     model = build_model(vocab_size, embedding_dim, rnn_units, BATCH_SIZE)
+
+    for input_example_batch, target_example_batch in dataset.take(1):
+        example_batch_predictions = model(input_example_batch)
+        print(example_batch_predictions.shape, "# (batch_size, sequence_length, vocab_size)")
+
     model.summary()
 
     def loss(labels, logits):
@@ -93,11 +107,12 @@ if not args.skip:
     model.compile(optimizer='adam', loss=loss)
 
     # Execute training
-    EPOCHS = 10
+    EPOCHS = 50
     history = model.fit(dataset, epochs=EPOCHS, callbacks=[checkpoint_callback])
 
 
 # Generate the text
+# tf.train.latest_checkpoint(checkpoint_dir)
 model = build_model(vocab_size, embedding_dim, rnn_units, batch_size=1)
 model.load_weights(tf.train.latest_checkpoint(checkpoint_dir))
 model.build(tf.TensorShape([1, None]))
@@ -107,14 +122,14 @@ def generate_text(model, start_string):
     # Number of characters to generate
     num_generate = 1000
 
-    input_ids = [char2idx[c] for c in start_string]
+    input_ids = [char2idx[s] for s in start_string]
     input_ids = tf.expand_dims(input_ids, 0)
 
     text_generated = []
 
     # Low - results in more predictable text
     # High - more surprising text
-    temperature = 1
+    temperature = 1.0
 
     model.reset_states()
     for i in range(num_generate):
@@ -126,11 +141,11 @@ def generate_text(model, start_string):
         predicted_id = tf.random.categorical(predictions, num_samples=1)[-1, 0].numpy()
 
         # Pass the predicted character as the next input to the model
-        input_eval = tf.expand_dims([predicted_id], 0)
+        input_ids = tf.expand_dims([predicted_id], 0)
         text_generated.append(idx2char[predicted_id])
 
     return start_string + ''.join(text_generated)
 
 
-print(generate_text(model, 'Have '))
+print(generate_text(model, '[Verse 1] '))
 
